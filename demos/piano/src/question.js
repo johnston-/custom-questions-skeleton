310c4f86a8ec7b056489e28bc55047d188b12a44
michael-fares-lrn <michael.fares@learnosity.com>
2022-11-11 14:35:45 -0600

    [FIX] remove piano directory from this branch

--- demos/piano/src/question.js
+++
@@ -1,9 +0,0 @@
-import PianoQuestion from './question/index';
-import '../scss/main.scss';
-
-/*global LearnosityAmd*/
-LearnosityAmd.define([], function () {
-    return {
-        Question: PianoQuestion
-    };
-});
--- demos/piano/src/scorer.js
+++
@@ -1,8 +0,0 @@
-/*globals LearnosityAmd*/
-import PianoScorer from './scorer/index';
-
-LearnosityAmd.define([], function () {
-    return {
-        Scorer: PianoScorer
-    };
-});
